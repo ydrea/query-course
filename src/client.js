e0c07cb3208abacc58ab73dcb2d3e4cd8b88c81c
ydrea <andrija.wrld@gmail.com>
2021-06-17 18:12:44 +0200

    client.js

---
+++ src/client.js
@@ -0,0 +1,5 @@
+
+import {QueryClient, QueryClientProvider} from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+export const client = new QueryClient()
--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {QueryClient, QueryClientProvider} from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
-
-const client = new QueryClient()
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { client } from "./client";
 
 ReactDOM.render(
   <QueryClientProvider client={client}>
     <App />
-    <ReactQueryDevtools/>
+    <ReactQueryDevtools />
   </QueryClientProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
